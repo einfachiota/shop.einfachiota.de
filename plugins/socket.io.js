d09c809b6399a9033cfc99aad1dacfd4ffa2e3c8
huhn <sehe89@gmail.com>
2019-11-02 19:03:25 +0100

    fix path

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -4,11 +4,5 @@ import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io-extended'
 
 export default ({ store }) => {
-  Vue.use(
-    VueSocketIO,
-    io('https://store.einfachiota.de', { path: '/api/iota_payments/socket' }),
-    {
-      store
-    }
-  )
+  Vue.use(VueSocketIO, io('https://store.einfachiota.de', {}), { store })
 }
