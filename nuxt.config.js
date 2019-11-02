36724af0c187b799bcd5a9f08f8ce7812d1c381b
huhn <sehe89@gmail.com>
2019-11-02 18:32:27 +0100

    change proxi

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ module.exports = {
     proxy: true // Can be also an object with default options
   },
   proxy: {
-    '/api': { target: 'https://store.einfachiota.de/', ws: true }
+    '/api': { target: 'http://localhost:3000', ws: true }
   },
   /*
    ** Build configuration
