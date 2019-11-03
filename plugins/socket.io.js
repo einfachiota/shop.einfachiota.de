d83a3d412c3f3399cc431cb0292ec384a8ce4c0d
huhn <sehe89@gmail.com>
2019-11-03 04:06:28 +0100

    add socket io config

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -6,7 +6,13 @@ import VueSocketIO from 'vue-socket.io-extended'
 export default ({ store }) => {
   Vue.use(
     VueSocketIO,
-    io('https://store.einfachiota.de', { path: '/api/iota_payments/socket' }),
+    io('https://store.einfachiota.de', {
+      path: '/api/iota_payments/socket',
+      log: false,
+      agent: false,
+      origins: '*:*',
+      transports: ['websocket', 'xhr-polling', 'jsonp-polling', 'polling']
+    }),
     {
       store
     }
