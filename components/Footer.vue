0a6014f9279d43bf71643de1d88ab994f1d53042
Sven Reiser <svreiser@gmail.com>
2019-12-21 20:14:35 +0100

    extends footer to full width

--- components/Footer.vue
+++ components/Footer.vue
@@ -80,6 +80,9 @@
   border-top: 5px solid var(--primary);
   color: var(--dark);
   margin-top: 100px;
+  margin-left: -50px;
+  width: calc(100% + 100px);
+  margin-bottom: -95px;
   .row {
     display: flex;
     justify-content: space-between;
