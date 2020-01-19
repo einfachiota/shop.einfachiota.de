f1750d83610cb40aed4d05ca0b9facbeda4c68ca
huhn511 <sehe89@gmail.com>
2020-01-19 15:51:24 +0100

    refactor translation files to seperate directory

---
+++ translations/de.js
@@ -0,0 +1,25 @@
+module.exports = {
+  greeting: 'Hallo Welt!',
+  order: {
+    form: {
+      first_name_label: 'Vorname',
+      first_name_placeholder: 'Dein Vorname',
+      last_name_label: 'Nachname',
+      last_name_placeholder: 'Dein Nachname',
+      address_label: 'Adresse',
+      address_placeholder: 'Deine Adresse und Hausnummer',
+      zip_code_label: 'Postleitzahl',
+      zip_code_placeholder: 'Deine Postleitzahl',
+      city_label: 'Stadt',
+      city_placeholder: 'Deine Stadt',
+      country_label: 'Land',
+      country_placeholder: 'Wähle dein Land',
+      amount_label: 'Menge der Magazine',
+      email_label: 'E-Mail',
+      email_placeholder:
+        'Deine E-Mail Adresse, falls bei der Lieferung etwas schief geht.',
+      newsletter_label: 'Neuigkeiten per E-Mail',
+      newsletter_placeholder: 'Ich möchte Neuigkeiten erhalten.'
+    }
+  }
+}
---
+++ translations/en.js
@@ -0,0 +1,24 @@
+module.exports = {
+  greeting: 'Hello world!',
+  order: {
+    form: {
+      first_name_label: 'First name',
+      first_name_placeholder: 'Your First name',
+      last_name_label: 'Last name',
+      last_name_placeholder: 'Your Last name',
+      address_label: 'Address',
+      address_placeholder: 'Your address with house number',
+      zip_code_label: 'Zip code',
+      zip_code_placeholder: 'Your zip code',
+      city_label: 'City',
+      city_placeholder: 'Your city',
+      country_label: 'Counry',
+      country_placeholder: 'Choose your country',
+      amount_label: 'Amount',
+      email_label: 'Email',
+      email_placeholder: 'Your email address',
+      newsletter_label: 'Neuigkeiten per E-Mail',
+      newsletter_placeholder: 'Ich möchte Neuigkeiten erhalten.'
+    }
+  }
+}
